d683a56794234361cb60643665628bf4447d451d
jeremiassosa <jeremias.sosa@gm2dev.com>
2026-08-28 09:35:00 -0300

    feat/ add complete exercise 1 lesson 8

--- react-bootcamp-Sosa/src/App.tsx
+++ react-bootcamp-Sosa/src/App.tsx
@@ -8,22 +8,21 @@ import ListForm from './lesson5/exercise2-ListForm';
 import DinamicList from './lesson5/exercise3-DinamixList';
 import LiveGreeting from './lesson6/exercise1-SaludoEnVivo';
 import LimitCaracters from './lesson6/exercise2-CampoConLimite';
+import CounterUseEffect from './lesson8/exercise1-ContadorClicks';
 
 
 function App() {
   return (
     <>      
     <h2>Lesson 3</h2>
-      
     <hr />
       
-    <h3>Exercise 1</h3>
-      <Counter />
-    <h3>Exercise 2</h3>
-      <ILike />
-      
+      <h3>Exercise 1</h3>
+        <Counter />
+      <h3>Exercise 2</h3>
+        <ILike />
+        
     <h2>Lesson 4</h2>
-    
     <hr />
       
       <h3>Exercise 1</h3>
@@ -31,8 +30,7 @@ function App() {
       <h3>Exercise 2</h3>
         <AlertStock />
 
-    <h2>Lesson 6</h2>
-
+    <h2>Lesson 5</h2>
     <hr />
 
       <h3>Exercise 1</h3>
@@ -42,14 +40,19 @@ function App() {
       <h3>Exercise 3</h3>
         <DinamicList/>
 
-    <h2>Lesson5</h2>
-
+    <h2>Lesson 6</h2>
     <hr />
 
       <h3>Exercise 1</h3>
         <LiveGreeting/>
       <h3>Exercise 2</h3>
         <LimitCaracters/>
+    
+    <h2>Lesson 8</h2>
+    <hr />
+
+      <h3>Exercise 1</h3>
+       <CounterUseEffect/>
     </>
     )
 }
---
+++ react-bootcamp-Sosa/src/lesson8/exercise1-ContadorClicks.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+
+function CounterUseEffect() {
+  const [count, setCount] = useState(0)
+  
+  useEffect(() => {
+    document.title = `L8 - Click ${count}`
+  },[count])
+
+  return (
+    <>
+      <section id="center">
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          +
+        </button>
+        <span>Conunt is {count}</span>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count - 1)}
+        >
+          -
+        </button>
+      </section>
+    </>
+  )
+}
+export default CounterUseEffect
