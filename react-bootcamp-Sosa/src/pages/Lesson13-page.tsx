b1277dbe4bfb1cb64271f0d597087172d10ff38c
jeremiassosa <jeremias.sosa@gm2dev.com>
2026-09-02 10:05:40 -0300

    feat/ add complete exercise 1 lesson 13

---
+++ react-bootcamp-Sosa/src/pages/Lesson13-page.tsx
@@ -0,0 +1,13 @@
+import UseRefInput from "../lesson13/exercise1-UseRef";
+
+export function Page13() {
+    return (
+        <>
+        <h2>Lesson 16</h2>
+
+        <h3>Exercise 1</h3>
+          <UseRefInput />
+
+        </>
+    )
+}
