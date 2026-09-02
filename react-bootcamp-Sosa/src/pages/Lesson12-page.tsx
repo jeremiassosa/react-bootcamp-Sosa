7c9063a308128465c8c6f202d9fa74cafa9833c1
jeremiassosa <jeremias.sosa@gm2dev.com>
2026-09-02 09:22:58 -0300

    feat/ add complete exercise 3 lesson 12

--- react-bootcamp-Sosa/src/pages/Lesson12-page.tsx
+++ react-bootcamp-Sosa/src/pages/Lesson12-page.tsx
@@ -1,6 +1,8 @@
 import { ToggleThemeProvider } from "../lesson12/exercise1-UseToggle";
-import { ToggleThemeWithContext } from "../lesson12/exercise1-UseToggleWihtContext";
-import { ComponenteContador } from "../lesson12/exercise2-UseCounterwithContext";
+import { ToggleThemeWithContext } from "../lesson12/exercise1-UseToggleWithContext";
+import { ComponenteContador } from "../lesson12/exercise2-UseCounterwithZustand";
+import { ToggleLocalThemeProvider } from "../lesson12/exercise3-UseLocalStorage";
+import { LocalThemeWithContext } from "../lesson12/exercise3-UseLocalStorageRender";
 
 export function Page12() {
     return(
@@ -13,6 +15,10 @@ export function Page12() {
         </ToggleThemeProvider>
           <h3>Exercise 2</h3>
             <ComponenteContador/>
+          <h3>Exercise 3</h3>
+        <ToggleLocalThemeProvider>
+            <LocalThemeWithContext/>
+        </ToggleLocalThemeProvider>
         </>
     )
 }
